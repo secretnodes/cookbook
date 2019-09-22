4eed01f19945da8eb3c8601b345ee72a4817544a
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-21 21:34:28 -0700

    fix

--- install-sgx.sh
+++ install-sgx.sh
@@ -2,33 +2,32 @@
 #Tested on ubuntu server 18.04
 #Version 0.1
 
-echo "To install the Intel® SGX PSW, first install the following tools: On Ubuntu* 18.04:"
+echo "===> Running step 1"
 sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
 
-echo "To install the Intel® SGX SDK, install the following: On Ubuntu* 18.04:"
+echo "===> Running step 2"
 sudo apt-get install build-essential python
 
-echo "===> Downloading Intel SGX Driver Files."
+echo "===> Running step 3"
 wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb"
-wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dbgsym_2.6.100.51363-bionic1_amd64.ddeb"
 wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb"
 wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_driver_2.5.0_2605efa.bin"
 wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_sdk_2.6.100.51363.bin"
 
-echo "===> Change permissions for sgx driver bin."
+echo "===> Running step 4"
 chmod +x ./sgx_linux_x64_driver_2.5.0_2605efa.bin
-chmod +x sgx_linux_x64_sdk_2.6.100.51363.bin
+chmod +x ./sgx_linux_x64_sdk_2.6.100.51363.bin
 
-echo "Install the Intel® SGX driver package"
+echo "===> Running step 5 - installing driver"
 sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
 
-echo "Install the Intel® SGX PSW package using the following command: on Ubuntu* 18.04:"
+echo "===> Running step 6 - installing psw"
 sudo dpkg -i ./libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
 sudo dpkg -i libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb
 
-echo "Installing sgx sdk."
-sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+echo "===> Running step 7 - installing sdk"
+sudo ./sgx_linux_x64_sdk_2.6.100.51363.bin
 
-echo "===> Installed SGX on Linux."
+echo "===> Installed SGX on Linux. Chose to install SGX in the 'isgx' directory. So answer 'no' then type 'isgx' without quotes."
 echo "Setting LD LIBRARY PATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
