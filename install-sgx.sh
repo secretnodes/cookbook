c188d58f918e50f26096c213d94e6478606d6924
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-21 20:23:06 -0700

    create install-sgx.sh

---
+++ install-sgx.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Tested on ubuntu server 18.04
+#Version 0.1
+
+echo "To install the Intel® SGX PSW, first install the following tools: On Ubuntu* 18.04:"
+sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
+
+echo "To install the Intel® SGX SDK, install the following: On Ubuntu* 18.04:"
+sudo apt-get install build-essential python
+
+echo "===> Downloading Intel SGX Driver Files."
+wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb"
+wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dbgsym_2.6.100.51363-bionic1_amd64.ddeb"
+wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb"
+wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_driver_2.5.0_2605efa.bin"
+wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_sdk_2.6.100.51363.bin"
+
+echo "===> Change permissions for sgx driver bin."
+chmod +x ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+chmod +x ./sgx_linux_x64_sdk_2.6.100.51363.bin
+
+echo "Install the Intel® SGX driver package"
+sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+
+echo "Install the Intel® SGX PSW package using the following command: on Ubuntu* 18.04:"
+sudo dpkg -i ./libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
+
+echo "Ubuntu* 18.04 requires installing this debug symbol package."
+sudo dpkg -i ./libsgx-enclave-common-dbgsym_2.6.100.51363-bionic1_amd64.ddeb
+
+echo "===> Running step 7 - installing sdk"
+sudo ./sgx_linux_x64_sdk_2.6.100.51363.bin
+
+echo "===> Installed SGX on Linux. Chose to install SGX in the 'isgx' directory. So answer 'no' then type 'isgx' without quotes."
+echo "Setting LD LIBRARY PATH"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
