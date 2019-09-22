92341689182df0afa221dbe1dd5f263c7230a9f0
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-22 08:06:34 -0700

    update sendnodes.sh
    Updated to Version 0.15

--- sendnodes.sh
+++ sendnodes.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.1
+#Version 0.15 | Sep 22, 2019
 
 echo "Downloading scripts to configure your Secret Node."
-wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh"
+wget -O "https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh"
 sleep 1
-wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh"
-sleep 1
-wget "https://raw.githubusercontent.com/secretnodes/scripts/master/start.sh"
+wget -O "https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh"
 sleep 1
 
 echo "Downloading Discovery-Docker-Network."
-git clone https://github.com/enigmampc/discovery-docker-network.git
+git -O clone https://github.com/enigmampc/discovery-docker-network.git
 sleep 1
 
 echo "<3 from https://secretnodes.org"
