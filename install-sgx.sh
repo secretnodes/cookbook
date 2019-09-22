6d6bd7bafa30c686186b90c8d5e0a87a1413ba8f
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-21 22:36:20 -0700

    fix

--- install-sgx.sh
+++ install-sgx.sh
@@ -1,33 +1,47 @@
-#!/bin/bash
-#Tested on ubuntu server 18.04
-#Version 0.1
+    #!/bin/bash
+    #Tested on ubuntu server 18.04
+    #Version 0.1
 
-echo "===> Running step 1"
-sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
+    echo "apt-get dpkg & update"
+    sudo apt-get update
+    sleep 5
+    apt install dpkg
 
-echo "===> Running step 2"
-sudo apt-get install build-essential python
+    echo "To install the Intel® SGX PSW, first install the following tools"
+    sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
 
-echo "===> Running step 3"
-wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb"
-wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb"
-wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_driver_2.5.0_2605efa.bin"
-wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_sdk_2.6.100.51363.bin"
+    echo "To install the Intel® SGX SDK, install the following:"
+    sudo apt-get install build-essential python
 
-echo "===> Running step 4"
-chmod +x ./sgx_linux_x64_driver_2.5.0_2605efa.bin
-chmod +x ./sgx_linux_x64_sdk_2.6.100.51363.bin
+    echo "Downloading the required SGX files."
+    wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb"
+    sleep 2
+    wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb"
+    sleep 2
+    wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_driver_2.5.0_2605efa.bin"
+    sleep 2
+    wget "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_sdk_2.6.100.51363.bin"
 
-echo "===> Running step 5 - installing driver"
-sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+    echo "Setting permissions for driver & SDK."
+    chmod +x ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+    sleep 2
+    chmod +x ./sgx_linux_x64_sdk_2.6.100.51363.bin
+    sleep 1
 
-echo "===> Running step 6 - installing psw"
-sudo dpkg -i ./libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
-sudo dpkg -i libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb
+    echo "Installing SGX driver."
+    sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
+    sleep 4
 
-echo "===> Running step 7 - installing sdk"
-sudo ./sgx_linux_x64_sdk_2.6.100.51363.bin
+    echo "Installing psw"
+    sudo dpkg -i ./libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
+    sleep 2
+    sudo d2kg -i libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb
+    sleep 1
 
-echo "===> Installed SGX on Linux. Chose to install SGX in the 'isgx' directory. So answer 'no' then type 'isgx' without quotes."
-echo "Setting LD LIBRARY PATH"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+    echo "===> Running step 7 - installing sdk"
+    sudo ./sgx_linux_x64_sdk_2.6.100.51363.bin
+    sleep 2
+
+    echo "===> Installed SGX on Linux. Chose to install SGX in the 'isgx' directory. So answer 'no' then type 'isgx' without quotes."
+    echo "Setting LD LIBRARY PATH"
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
