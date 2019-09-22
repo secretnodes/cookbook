a377d658e4a0dd2d1d31986b4e13b882eb4f515f
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-21 22:43:28 -0700

    update sendnodes.sh

--- sendnodes.sh
+++ sendnodes.sh
@@ -3,7 +3,9 @@
 
 echo "Downloading scripts to configure your Secret Node."
 wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh"
+sleep 1
 wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh"
+sleep 1
 
 echo "Downloading Discovery-Docker-Network."
 git clone https://github.com/enigmampc/discovery-docker-network.git
