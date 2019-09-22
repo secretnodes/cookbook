6c29a8e013caa6238d11fd8b8452f127f31bdad6
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-21 20:35:48 -0700

    update

--- grab-scripts.sh
+++ grab-scripts.sh
@@ -2,5 +2,5 @@
 #Configured for ubuntu server 18.04
 
 echo "Downloading scripts to configure your Secret Node."
-wget "https://github.com/secretnodes/scripts/blob/master/install-docker.sh"
-wget "https://github.com/secretnodes/scripts/blob/master/install-sgx.sh"
+wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh"
+wget "https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh"
