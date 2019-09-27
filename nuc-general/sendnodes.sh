3d1917bace4a1418a4e2cf80cd7278fcc6d6dd0c
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-26 20:30:24 -0700

    fixed doctype issue
    switched to raw script links

--- nuc-general/sendnodes.sh
+++ nuc-general/sendnodes.sh
@@ -4,19 +4,19 @@
 #Tested on : 8i7BEK, ...
 
 echo "Downloading scripts to configure your Secret Node."
-wget -O install-docker.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/install-docker.sh
+wget -O install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-docker.sh
 sleep 1
-wget -O install-sgx.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/install-sgx.sh
+wget -O install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-sgx.sh
 sleep 1
-wget -O start.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/start.sh
+wget -O start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/start.sh
 sleep 1
-wget -O install-enigma-node.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/install-enigma-node.sh
+wget -O install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-enigma-node.sh
 sleep 1
-wget -O upgrade.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/upgrade.sh
+wget -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/upgrade.sh
 sleep 1
-wget -O upgrade.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/install-fixes.sh
+wget -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-fixes.sh
 sleep 1
-wget -O upgrade.sh https://github.com/secretnodes/scripts/blob/master/nuc-general/uninstall.sh
+wget -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/uninstall.sh
 sleep 1
 
 echo "Change permissions for install sgx, install docker, start, & upgrade scripts."
