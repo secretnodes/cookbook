fddd346f7f414a5f0700fa653e4ce37fe566e1e4
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-26 19:50:21 -0700

    Update install-fixes.sh

--- nuc-general/install-fixes.sh
+++ nuc-general/install-fixes.sh
@@ -3,9 +3,11 @@
 #Version 0.2 NUC General | Sep 26, 2019
 #Tested on : 8i7BEK, ...
 
-Cat "nameserver 1.1.1.1
-nameserver 8.8.8.8
-nameserver 127.0.0.53
-options edns0t" | tee nuc-1.fix
+#Cat "nameserver 1.1.1.1
+#nameserver 8.8.8.8
+#nameserver 127.0.0.53
+#options edns0t" | tee nuc-1.fix
 
-echo "Created nuc-1.fix for docker proxy error. This is currently an experimental fix."
+#echo "Created nuc-1.fix for docker proxy error. This is currently an experimental fix."
+
+echo "No current fixes turned on. Run sendnodes.sh to upgrade all scripts and see if a fix is available."
