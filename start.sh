661661ffef2da1c71ca0e03746ad12cd5d449e70
moonstash <hello@secretnodes.org>
2019-09-26 19:16:04 -0700

    reorg
    breaks old scripts, will fix old scripts in next push.

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Configured for ubuntu server 18.04
-#Version 0.1 | Sep 22, 2019
-
-echo "CD into Discovery-Docker-Network"
-cd discovery-docker-network
-
-echo "Start 9 workers on your Secret Node. Note : 9 is the max amount of workers you can run."
-sudo NODES=9 ./launch.bash
