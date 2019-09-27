bf234b4e82a153be3fa2238b0c66a095d996e640
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-26 19:33:34 -0700

    New Script: Downloads enigma software
    Script to download the relevant enigma software to run a node. Currently it downloads what is needed to launch a devnet. Eventually this will be changed.

---
+++ nuc-general/install-enigma-node.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Configured for ubuntu server 18.04
+#Version 0.2 NUC General | Sep 26, 2019
+#Tested on : 8i7BEK, ...
+
+echo "Downloading and installing enigma node software."
+sudo bash install-enigma-node.sh
