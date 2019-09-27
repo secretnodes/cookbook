4d08cb21c5360fe6cad150e0eecb9e9f0928ea7b
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-26 19:48:47 -0700

    created install-fixes.sh
    Experimenting with fixes. Not implemented live yet.

---
+++ nuc-general/install-fixes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Configured for ubuntu server 18.04
+#Version 0.2 NUC General | Sep 26, 2019
+#Tested on : 8i7BEK, ...
+
+Cat "nameserver 1.1.1.1
+nameserver 8.8.8.8
+nameserver 127.0.0.53
+options edns0t" | tee nuc-1.fix
+
+echo "Created nuc-1.fix for docker proxy error. This is currently an experimental fix."
