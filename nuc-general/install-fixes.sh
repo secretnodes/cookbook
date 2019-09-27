32f95c0e1fe37290d8dd039f307f9e29be68be17
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-26 20:11:43 -0700

    Update install-fixes.sh

--- nuc-general/install-fixes.sh
+++ nuc-general/install-fixes.sh
@@ -3,11 +3,14 @@
 #Version 0.2 NUC General | Sep 26, 2019
 #Tested on : 8i7BEK, ...
 
-#Cat "nameserver 1.1.1.1
-#nameserver 8.8.8.8
-#nameserver 127.0.0.53
-#options edns0t" | tee nuc-1.fix
+echo "Created nuc-1.fix"
+echo "This is currently an experimental fix for issue Get https://registry-1.docker.io/v2/: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)"
+echo "It creates a file with new nameservers to symlink to /etc/resolv.conf"
+echo "Please report any issues you face at https://t.me/secretnodes"
 
-#echo "Created nuc-1.fix for docker proxy error. This is currently an experimental fix."
+Cat "nameserver 1.1.1.1
+nameserver 8.8.8.8
+nameserver 127.0.0.53
+options edns0t" | tee nuc-1.fix
 
-echo "No current fixes turned on. Run sendnodes.sh to upgrade all scripts and see if a fix is available."
+echo "Note: To upgrade all scripts, you must merely run sendnodes.sh again."
