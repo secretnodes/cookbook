93f568537f29262703a776a989178f2619e0a65a
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-27 17:45:17 -0700

    Update sendnodes.sh

--- nuc-general/sendnodes.sh
+++ nuc-general/sendnodes.sh
@@ -4,19 +4,19 @@
 #Tested on : 8i7BEK, ...
 
 echo "Downloading scripts to configure your Secret Node."
-wget -O install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-docker.sh
+curl -O install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-docker.sh
 sleep 1
-wget -O install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-sgx.sh
+curl -O install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-sgx.sh
 sleep 1
-wget -O start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/start.sh
+curl -O start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/start.sh
 sleep 1
-wget -O install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-enigma-node.sh
+curl -O install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-enigma-node.sh
 sleep 1
-wget -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/upgrade.sh
+curl -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/upgrade.sh
 sleep 1
-wget -O install-fixes.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-fixes.sh
+curl -O install-fixes.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/install-fixes.sh
 sleep 1
-wget -O uninstall.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/uninstall.sh
+curl -O uninstall.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-general/uninstall.sh
 sleep 1
 
 echo "Change permissions for install sgx, install docker, start, & upgrade scripts."
@@ -37,24 +37,24 @@ sleep 1
 
 echo "Running upgrade.sh script."
 sudo bash upgrade.sh
-sleep 5
+sleep 2
 echo "The script is not finished yet. Next we will install docker, please wait."
 
 echo "Running install-docker.sh script."
 sudo bash install-docker.sh
-sleep 5
+sleep 2
 echo "The script is not finished yet. Next we will install sgx, please wait."
 
 echo "Running install-sgx.sh script."
 sudo bash install-sgx.sh
-sleep 5
+sleep 2
 
 echo "Running install-enigma-node.sh"
 sudo bash install-enigma-node.sh
-sleep 5
+sleep 2
 
 echo "Running install-fixes.sh"
 sudo bash install-fixes.sh
-sleep 5
+sleep 2
 
 echo "<3 from https://secretnodes.org"
