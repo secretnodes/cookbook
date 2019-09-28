f4ecd85b7e01fc41c95f52e19750b1ad271bb841
Ian <15162813+moonstash@users.noreply.github.com>
2019-09-27 18:39:08 -0700

    Update install-fixes.sh

--- nuc-general/install-fixes.sh
+++ nuc-general/install-fixes.sh
@@ -3,12 +3,16 @@
 #Version 0.2 NUC General | Sep 26, 2019
 #Tested on : 8i7BEK, ...
 
-echo "Created nuc-1.fix"
 echo "This is currently an experimental fix for issue Get https://registry-1.docker.io/v2/: net/http: request canceled while waiting for connection (Client.Timeout exceeded while awaiting headers)"
-echo "It creates a file with new nameservers to symlink to /etc/resolv.conf"
 echo "Please report any issues you face at https://t.me/secretnodes"
 
-echo "nameserver 1.1.1.1\nnameserver 8.8.8.8\nnameserver 127.0.0.53\noptions edns0t" >> nuc-1.fix
+echo "Installing dnsmasq"
+sudo apt-get install dnsmasq
+
+echo "Adding 1.1.1.1 cloudflare nameserver."
+echo "server=1.1.1.1" >> nuc-1.fix
+sudo cp /etc/dnsmasq.conf dnsmasq-default.conf
+sudo mv -f nuc-1.fix /etc/dnsmasq.conf
 
 echo "Creating symlink via: ln -s /etc/resolv.conf nuc-1.fix"
 ln -s /etc/resolv.conf nuc-1.fix
