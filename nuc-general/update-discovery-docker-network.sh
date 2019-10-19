eb4feb7f11b5ff3b3061cc0c7e9e3d9d9f68a867
moonstash <hello@secretnodes.org>
2019-10-18 17:54:02 -0700

    added update script
    added update script for enigma-docker-network

---
+++ nuc-general/update-discovery-docker-network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Configured for ubuntu server 18.04
+
+echo "Updating enigma node software."
+echo "Pre Genesis Games | DevNet | Discovery-Docker-Network."
+cd discovery-docker-network
+docker-compose pull
+sleep 2
