f7cac507d33417ddc54bcc7557dce0e131bd6a18
moonstash <15162813+moonstash@users.noreply.github.com>
2019-10-30 16:05:15 -0700

    updated install-enigma-node.sh
    updated to replace current install if present

--- nuc-general/install-enigma-node.sh
+++ nuc-general/install-enigma-node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.2 NUC General | Sep 26, 2019
+#Version 0.21 NUC General | Oct 30, 2019
 #Tested on : 8i7BEK, ...
 
 echo "Downloading and installing enigma node software."
 echo "Pre Genesis Games | DevNet | Discovery-Docker-Network."
-git clone https://github.com/enigmampc/discovery-docker-network.git
+git clone https://github.com/enigmampc/discovery-docker-network.git tmp/
+cp -R discovery-docker-network/ tmp/
 sleep 2
