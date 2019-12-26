11f5980bcdd8ce20ba0a0151e60285a4c3c2a1e2
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 13:00:28 -0800

    Update update-discovery-testnet.sh
    added logging

--- nuc-discovery-testnet/update-discovery-testnet.sh
+++ nuc-discovery-testnet/update-discovery-testnet.sh
@@ -2,8 +2,9 @@
 #Configured for ubuntu server 18.04
 #Tested on : 8i7BEK, ...
 # This is for the discovery testnet beta, NOTE THE NETWORK HAS NOT LAUNCHED.
+# V0.12 | Dec 26, 2019
 
-echo "Updating enigma node software."
+echo $(date -u) "Updating enigma node software." >> sendlogs.txt
 echo "Discovery Testnet Beta."
-docker pull enigmampc/external_worker_hw:testnet
+docker pull enigmampc/external_worker_hw:testnet >> sendlogs.txt
 sleep 1
