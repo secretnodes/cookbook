9eeea87fb0ec82f078469f829d75644401f79309
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 05:27:09 -0800

    update start.sh
    extended update abilities

--- nuc-discovery-testnet/start.sh
+++ nuc-discovery-testnet/start.sh
@@ -2,6 +2,23 @@
 #Configured for ubuntu server 18.04
 #Version 0.1 | Sep 22, 2019
 
+echo "Updating all scripts for configuring your Secret Node."
+wget -O install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-docker.sh
+sleep 1
+wget -O install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-sgx.sh
+sleep 1
+wget -O start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/start.sh
+sleep 1
+wget -O install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-enigma-node.sh
+sleep 1
+wget -O upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/upgrade.sh
+sleep 1
+wget -O install-fixes.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-fixes.sh
+sleep 1
+wget -O update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/update-discovery-testnet.sh
+sleep 1
+wget -O update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/cli.sh
+
 echo "Making sure discovery-docker-network is up to date."
 bash update-discovery-testnet.sh
 
