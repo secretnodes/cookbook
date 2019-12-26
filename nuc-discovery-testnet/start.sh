04db2faef5a847082b3e471cb831a1728968ea4b
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 05:58:05 -0800

    Update start.sh

--- nuc-discovery-testnet/start.sh
+++ nuc-discovery-testnet/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.1 | Sep 22, 2019
+#Version 0.15 | Dec 26, 2019
 
 echo "Checking for updates then running upgrade script."
 wget -N upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/upgrade.sh
