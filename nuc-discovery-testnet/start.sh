87665f16b19fecff2e31e825aa0a9acc94bbb41b
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 12:57:46 -0800

    Update start.sh
    added logging

--- nuc-discovery-testnet/start.sh
+++ nuc-discovery-testnet/start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.15 | Dec 26, 2019
+#Version 0.16 | Dec 26, 2019
 
-echo "Checking for updates then running upgrade script."
+echo $(date -u) "Checking for updates then running upgrade script." >> sendlogs.txt
 wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/upgrade.sh
 sleep 1
 bash upgrade.sh
 sleep 1
 
-echo "Launch discovery testnet worker."
+echo $(date -u) "Launch discovery testnet worker." >> sendlogs.txt
 cd discovery-testnet
 sleep 1
 docker-compose up
 sleep 1
 
-echo "Next launch the command-line interface (CLI) for your node"
+echo $(date -u) "Next launch the command-line interface (CLI) for your node" >> sendlogs.txt
 echo "by opening a new terminal window and running cli.sh"
