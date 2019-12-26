02de862e265ac8d321962db4297861fc3167143c
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 12:53:30 -0800

    Update install-fixes.sh
    added logging

--- nuc-discovery-testnet/install-fixes.sh
+++ nuc-discovery-testnet/install-fixes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.2 NUC General | Sep 26, 2019
+#Version 0.22 NUC General | Dec 26, 2019
 #Tested on : 8i7BEK, ...
 
 echo "Please report any issues you face at https://t.me/secretnodes"
 
-echo "Implimenting fix for docker caused networking issue https://github.com/docker/libnetwork/issues/2187"
+echo $(date -u) "Implimenting fix for docker caused networking issue https://github.com/docker/libnetwork/issues/2187" >> sendlogs.txt
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-echo "Note: To upgrade all scripts, you must merely run sendnodes.sh again."
+echo $(date -u) "Note: To upgrade all scripts, you must merely run sendnodes.sh again." >> sendlogs.txt
