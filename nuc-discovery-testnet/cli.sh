3e0313f0ee282d240f34585ddd1dba7c6ceca241
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 05:58:25 -0800

    Update cli.sh

--- nuc-discovery-testnet/cli.sh
+++ nuc-discovery-testnet/cli.sh
@@ -2,6 +2,7 @@
 #Configured for ubuntu server 18.04
 #Tested on : 8i7BEK, ...
 # This is for the discovery testnet beta, NOTE THE NETWORK HAS NOT LAUNCHED.
+#Version 0.15 | Dec 26, 2019
 
 echo "Updating start.sh script"
 wget -N start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/start.sh
