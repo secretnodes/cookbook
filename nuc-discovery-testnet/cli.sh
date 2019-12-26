e1e292d679218bc9f83629a6411a6465a294d9f8
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 05:28:27 -0800

    update cli.sh
    added update ability for other script.

--- nuc-discovery-testnet/cli.sh
+++ nuc-discovery-testnet/cli.sh
@@ -3,6 +3,10 @@
 #Tested on : 8i7BEK, ...
 # This is for the discovery testnet beta, NOTE THE NETWORK HAS NOT LAUNCHED.
 
+echo "Updating start.sh script"
+wget -O start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/start.sh
+sleep 1
+
 echo "Launching Discovery Testnet Beta CLI"
 cd discovery-testnet
 sleep 1
