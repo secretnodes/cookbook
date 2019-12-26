8f42b7c31e1267ef154e3050bfc3ded939bbe8c5
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 05:52:58 -0800

    update upgrade.sh
    moved all upgrade logic here.

--- nuc-discovery-testnet/upgrade.sh
+++ nuc-discovery-testnet/upgrade.sh
@@ -14,4 +14,21 @@ sleep 2
 sudo apt-get autoclean
 sleep 2
 
+echo "Updating all scripts for configuring your Secret Node."
+wget -N install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-docker.sh
+sleep 1
+wget -N install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-sgx.sh
+sleep 1
+wget -N install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-enigma-node.sh
+sleep 1
+wget -N install-fixes.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/install-fixes.sh
+sleep 1
+wget -N update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/update-discovery-testnet.sh
+sleep 1
+wget -N update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/cli.sh
+sleep 1
+wget -N start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/start.sh
+sleep 1
+
+
 echo "Complete!"
