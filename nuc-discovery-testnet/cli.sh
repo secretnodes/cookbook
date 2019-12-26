a180a91704c8f75470f24f0ce254ec71177dc396
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 12:49:20 -0800

    Update cli.sh
    added logging

--- nuc-discovery-testnet/cli.sh
+++ nuc-discovery-testnet/cli.sh
@@ -2,13 +2,13 @@
 #Configured for ubuntu server 18.04
 #Tested on : 8i7BEK, ...
 # This is for the discovery testnet beta, NOTE THE NETWORK HAS NOT LAUNCHED.
-#Version 0.15 | Dec 26, 2019
+#Version 0.152 | Dec 26, 2019
 
-echo "Updating start.sh script"
+echo $(date -u) "Updating start.sh script" >> sendlogs.txt
 wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/nuc-discovery-testnet/start.sh
 sleep 1
 
-echo "Launching Discovery Testnet Beta CLI"
+echo $(date -u) "Launching Discovery Testnet Beta CLI" >> sendlogs.txt
 cd discovery-testnet
 sleep 1
 docker exec -it worker cli
