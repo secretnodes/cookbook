e5f7fee0e1d1d70347f9b7fc9d6ea8f8b3aa0c5c
moonstash <15162813+moonstash@users.noreply.github.com>
2019-12-26 12:55:19 -0800

    Update install-sgx.sh
    added logging

--- nuc-discovery-testnet/install-sgx.sh
+++ nuc-discovery-testnet/install-sgx.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Tested on ubuntu server 18.04
-#Version 0.1 | Sep 22, 2019
+#Version 0.12 | Dec 26, 2019
 
 echo "apt-get dpkg & update"
 sudo apt-get update
 sleep 5
 sudo apt install dpkg
 
-echo "To install the Intel® SGX PSW, first install the following tools"
+echo $(date -u) "To install the Intel® SGX PSW, first install the following tools" >> sendlogs.txt
 sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
 
-echo "To install the Intel® SGX SDK, install the following:"
+echo $(date -u) "To install the Intel® SGX SDK, install the following:" >> sendlogs.txt
 sudo apt-get install build-essential python
 
-echo "Downloading the required SGX files."
+echo $(date -u) "Downloading the required SGX files." >> sendlogs.txt
 curl -O "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb"
 sleep 2
 curl -O "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb"
@@ -22,26 +22,26 @@ curl -O "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linu
 sleep 2
 curl -O "https://download.01.org/intel-sgx/linux-2.6/ubuntu18.04-server/sgx_linux_x64_sdk_2.6.100.51363.bin"
 
-echo "Setting permissions for driver & SDK."
+echo $(date -u) "Setting permissions for driver & SDK." >> sendlogs.txt
 chmod +x ./sgx_linux_x64_driver_2.5.0_2605efa.bin
 sleep 2
 chmod +x ./sgx_linux_x64_sdk_2.6.100.51363.bin
 sleep 1
 
-echo "Installing SGX driver."
+echo $(date -u) "Installing SGX driver." >> sendlogs.txt
 sudo ./sgx_linux_x64_driver_2.5.0_2605efa.bin
 sleep 4
 
-echo "Installing psw"
+echo $(date -u) "Installing psw" >> sendlogs.txt
 sudo dpkg -i ./libsgx-enclave-common_2.6.100.51363-bionic1_amd64.deb
 sleep 2
 sudo dpkg -i ./libsgx-enclave-common-dev_2.6.100.51363-bionic1_amd64.deb
 sleep 1
 
-echo "Installing SGX on Linux. Chose to install SGX in current directory, respond with 'no'."
+echo $(date -u) "Installing SGX on Linux. Chose to install SGX in current directory, respond with 'no'." >> sendlogs.txt
 sudo ./sgx_linux_x64_sdk_2.6.100.51363.bin
 sleep 2
 
-echo "Setting LD LIBRARY PATH"
+echo $(date -u) "Setting LD LIBRARY PATH" >> sendlogs.txt
 echo "<3 from https://secretnodes.org"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
