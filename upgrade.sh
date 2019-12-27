580c5175cb66c0fb21b01e4c83318cbc171091ea
moonstash <hello@secretnodes.org>
2019-12-27 12:00:38 -0800

    Cleanup
    merged refresh-script and upgrad together

--- upgrade.sh
+++ upgrade.sh
@@ -17,19 +17,48 @@ sleep 2
 sudo apt-get autoclean
 sleep 2
 
-echo $(date -u) "Updating all scripts for configuring your Secret Node." >> sendlogs.txt
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh
+echo $(date -u) "Downloading scripts to configure your Secret Node." >> sendlogs.txt
+wget -N clear-scripts.sh https://raw.githubusercontent.com/secretnodes/scripts/master/sendnodes.sh
 sleep 1
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh
+wget -N install-docker.sh https://raw.githubusercontent.com/secretnodes/scripts/master/install-docker.sh
 sleep 1
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/install-enigma-node.sh
+wget -N install-sgx.sh https://raw.githubusercontent.com/secretnodes/scripts/master/install-sgx.sh
 sleep 1
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/install-fixes.sh
+wget -N start.sh https://raw.githubusercontent.com/secretnodes/scripts/master/start.sh
 sleep 1
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/update-discovery-testnet.sh
+wget -N install-enigma-node.sh https://raw.githubusercontent.com/secretnodes/scripts/master/install-enigma-node.sh
 sleep 1
-wget -N https://raw.githubusercontent.com/secretnodes/scripts/master/cli.sh
+wget -N upgrade.sh https://raw.githubusercontent.com/secretnodes/scripts/master/upgrade.sh
 sleep 1
+wget -N install-fixes.sh https://raw.githubusercontent.com/secretnodes/scripts/master/install-fixes.sh
+sleep 1
+wget -N update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/update-discovery-testnet.sh
+sleep 1
+wget -N update-discovery-testnet.sh https://raw.githubusercontent.com/secretnodes/scripts/master/cli.sh
+#wget -O uninstall.sh https://raw.githubusercontent.com/secretnodes/scripts/master/uninstall.sh
+#sleep 1
+
+echo $(date -u) "Change permissions for install sgx, install docker, start, & upgrade scripts." >> sendlogs.txt
+sudo chmod u+x ~/sendnodes.sh
+sleep 1
+sudo chmod u+x ~/install-sgx.sh
+sleep 1
+sudo chmod u+x ~/install-docker.sh
+sleep 1
+sudo chmod u+x ~/start.sh
+sleep 1
+sudo chmod u+x ~/upgrade.sh
+sleep 1
+sudo chmod u+x ~/install-enigma-node.sh
+sleep 1
+sudo chmod u+x ~/install-fixes.sh
+sleep 1
+sudo chmod u+x ~/update-discovery-testnet.sh
+sleep 1
+sudo chmod u+x ~/cli.sh
+sleep 1
+#sudo chmod u+x ~/uninstall.sh
+#sleep 1
 
 echo $(date -u) "Updating enigma testnet software." >> sendlogs.txt
 bash update-discovery-testnet.sh >> sendlogs.txt
