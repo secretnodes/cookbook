1dd9b37ea849f7a91d42f459692faa67990318a4
moonstash <hello@secretnodes.org>
2019-12-27 13:51:27 -0800

    Update uninstall-sgx.sh

--- uninstall-sgx.sh
+++ uninstall-sgx.sh
@@ -12,6 +12,10 @@ sudo rm -rf "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
 sudo /sbin/depmod
 sudo /bin/sed -i '/^isgx$/d' /etc/modules
 sudo rm -rf isgx
+rm libsgx-enclave-common-dev_2.7.101.3-bionic1_amd64.deb
+rm libsgx-enclave-common_2.7.101.3-bionic1_amd64.deb
+rm sgx_linux_x64_driver_2.6.0_4f5bb63.bin
+rm sgx_linux_x64_sdk_2.7.101.3.bin
 
 echo $(date -u) "The SGX Driver should now be successfully removed." >> sendlogs.txt
 echo "<3 from https://secretnodes.org"
