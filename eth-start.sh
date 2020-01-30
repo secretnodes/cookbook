44c07b94cc2944b6ff587207e48b2db16245ac8a
moonstash <ian@secretnodes.org>
2020-01-30 14:59:44 -0800

    Test #5
    Test #5

---
+++ eth-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Configured for ubuntu server 18.04
+#Version 0.9 | Dec 27, 2019
+#Tested on 8i7BEK & 8i3BEH
+#Should work for SGX compatible intel NUCs, Vultr Bare Metal.
+#Confirmed working on enigma.co testnet
+
+sudo docker run -h eth --name ethereum-kovan -p 8546:8546 \
+           -v ~/.parity-kovan:/home/parity/.local/share/io.parity.ethereum/ \
+           -it parity/parity:stable --chain=kovan \
+           --ws-interface=all --ws-origins="all" \
+           --base-path /home/parity/.local/share/io.parity.ethereum/
