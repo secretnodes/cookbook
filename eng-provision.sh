90accc80d59af8c5ca9b038128ca8750a43d9301
moonstash <ian@secretnodes.org>
2020-01-30 16:49:11 -0800

    back to the future

--- eng-provision.sh
+++ eng-provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Configured for ubuntu server 18.04
-#Version 0.9 | Jan 30, 2020
+#Version 0.9 | Jan 31, 2020
 #Tested on 8i7BEK & 8i3BEH
 #Should work for SGX compatible intel NUCs, Vultr Bare Metal, (more to come)
 #Confirmed working on enigma.co testnet
